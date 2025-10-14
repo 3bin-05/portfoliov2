72b5adae74577fd760eb68740ba1a8852a5de3d5
3bin-05 <ebin05reji@gmail.com>
2025-10-14 06:41:42 +0530

    first commit

--- TODO.md
+++ TODO.md
@@ -1,9 +1,9 @@
-# TODO: Fix Errors in Portfolio Site
+# TODO: Fix Image Loading and Preloader Issues for Vercel Deployment
 
 ## Tasks to Complete
-- [ ] Fix Footer.jsx: Remove corrupted code in "Message Me" button and clean up social icons section
-- [ ] Fix Intro.jsx: Correct malformed SVG paths in social media links
-- [ ] Fix Works.jsx: Complete srcSet attribute for grayscale image and fix image paths
-- [ ] Test Changes: Run npm run dev and verify site loads without errors
-- [ ] Run npm run lint to check for ESLint errors
-- [ ] Test navigation, forms, and responsiveness
+- [ ] Import 2x image variants in Works.jsx and StylesPage.jsx
+- [ ] Update srcSet attributes in Works.jsx and StylesPage.jsx to use imported 2x images
+- [ ] Remove incorrect srcSet in Intro.jsx (no 2x variant available)
+- [ ] Modify Preloader.jsx to add a fallback timeout to handle image loading failures
+- [ ] Test Changes: Run npm run build and verify images load without errors
+- [ ] Deploy to Vercel and check if loading screen passes
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import gallery1 from '../assets/images/folio/caffeine_and_tulips.jpg';
+import gallery1_2x from '../assets/images/folio/caffeine_and_tulips@2x.jpg';
 import gallery2 from '../assets/images/folio/grayscale.jpg';
+import gallery2_2x from '../assets/images/folio/grayscale@2x.jpg';
 import gallery3 from '../assets/images/folio/lamp.jpg';
+import gallery3_2x from '../assets/images/folio/lamp@2x.jpg';
 import gallery4 from '../assets/images/folio/tropical.jpg';
+import gallery4_2x from '../assets/images/folio/tropical@2x.jpg';
 import gallery5 from '../assets/images/folio/white_turban.jpg';
+import gallery5_2x from '../assets/images/folio/white_turban@2x.jpg';
 import gallery6 from '../assets/images/folio/woodcraft.jpg';
+import gallery6_2x from '../assets/images/folio/woodcraft@2x.jpg';
 
 function Works() {
     return (
@@ -31,7 +37,7 @@ function Works() {
                     <div className="folio-list">
                         <div className="folio-list__item">
                             <div className="folio-list__item-media">
-                                <img src={gallery1} srcSet={`${gallery1} 1x, ../assets/images/folio/caffeine_and_tulips@2x.jpg 2x`} alt="" />
+                                <img src={gallery1} srcSet={`${gallery1} 1x, ${gallery1_2x} 2x`} alt="" />
                             </div>
                             <div className="folio-list__item-caption">
                                 <div className="folio-list__item-cat">Branding</div>
@@ -42,7 +48,7 @@ function Works() {
 
                         <div className="folio-list__item">
                             <div className="folio-list__item-media">
-                                <img src={gallery2} srcSet={`${gallery2} 1x, ../assets/images/folio/grayscale@2x.jpg 2x`} alt="" />
+                                <img src={gallery2} srcSet={`${gallery2} 1x, ${gallery2_2x} 2x`} alt="" />
                             </div>
                             <div className="folio-list__item-caption">
                                 <div className="folio-list__item-cat">Photography</div>
@@ -53,7 +59,7 @@ function Works() {
 
                         <div className="folio-list__item">
                             <div className="folio-list__item-media">
-                                <img src={gallery3} srcSet={`${gallery3} 1x, ../assets/images/folio/lamp@2x.jpg 2x`} alt="" />
+                                <img src={gallery3} srcSet={`${gallery3} 1x, ${gallery3_2x} 2x`} alt="" />
                             </div>
                             <div className="folio-list__item-caption">
                                 <div className="folio-list__item-cat">Product Design</div>
@@ -64,7 +70,7 @@ function Works() {
 
                         <div className="folio-list__item">
                             <div className="folio-list__item-media">
-                                <img src={gallery4} srcSet={`${gallery4} 1x, ../assets/images/folio/tropical@2x.jpg 2x`} alt="" />
+                                <img src={gallery4} srcSet={`${gallery4} 1x, ${gallery4_2x} 2x`} alt="" />
                             </div>
                             <div className="folio-list__item-caption">
                                 <div className="folio-list__item-cat">Branding</div>
@@ -75,7 +81,7 @@ function Works() {
 
                         <div className="folio-list__item">
                             <div className="folio-list__item-media">
-                                <img src={gallery5} srcSet={`${gallery5} 1x, ../assets/images/folio/white_turban@2x.jpg 2x`} alt="" />
+                                <img src={gallery5} srcSet={`${gallery5} 1x, ${gallery5_2x} 2x`} alt="" />
                             </div>
                             <div className="folio-list__item-caption">
                                 <div className="folio-list__item-cat">Photography</div>
@@ -86,7 +92,7 @@ function Works() {
 
                         <div className="folio-list__item">
                             <div className="folio-list__item-media">
-                                <img src={gallery6} srcSet={`${gallery6} 1x, ../assets/images/folio/woodcraft@2x.jpg 2x`} alt="" />
+                                <img src={gallery6} srcSet={`${gallery6} 1x, ${gallery6_2x} 2x`} alt="" />
                             </div>
                             <div className="folio-list__item-caption">
                                 <div className="folio-list__item-cat">Product Design</div>
